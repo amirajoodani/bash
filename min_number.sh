f9ef275ef3d7fd17f074a3f9d674d4891d4686f8
amirajoodani <42912741+amirajoodani@users.noreply.github.com>
2022-04-04 16:58:37 +0430

    find minimum number with bash
    find minimum number between 6 entire one with array in bash

---
+++ min_number.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+echo "enter your 6 numbers :"
+for i in {0..5};do
+    read  -p "please enter number $((i+1)) : " number
+    number_array[i]="$number"
+done
+
+min=${number_array[0]}
+
+for i in {1..5};do
+    if [ "${number_array[i]}" -lt  "$min" ];then
+        min=${number_array[i]}
+    fi
+done
+
+echo "minimun is $min"
+
